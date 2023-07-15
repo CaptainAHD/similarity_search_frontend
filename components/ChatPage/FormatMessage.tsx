8eb19ad5a9c9fe371f5d8cc1d5e2d39295116ff0
Tejas013 <tejasmadhukar6@gmail.com>
2023-07-15 15:08:09 +0530

    Done

--- components/ChatPage/FormatMessage.tsx
+++ components/ChatPage/FormatMessage.tsx
@@ -3,6 +3,8 @@ import { Divider } from "@nextui-org/react";
 import { FC } from "react";
 import Similarity from "./Buttons/Similarity";
 import Summary from "./Buttons/Summary";
+import { Button } from "@nextui-org/button";
+import { Link } from "@nextui-org/link";
 
 interface BeautifyProps {
     message: BotResponse[]
@@ -24,15 +26,14 @@ const Beautify:FC<BeautifyProps> = ({message,UserMessage}) => {
                     <p className="mr-2 font-bold">Facts: </p>
                     <p className="mr-2">{msg.Output.Facts}</p>
                 </div>
-                <div className="flex my-2">
-                    <p className="mr-2 font-bold">Summary: </p>
-                    <p className="mr-2">{msg.Output.CaseSummary}</p>
-                </div>
                 <div className="flex my-2 justify-center">
                     <Similarity message={msg} query={UserMessage} />
                 </div>
                 <div className="flex my-2 justify-center">
                     <Summary message={msg}/>
+                </div>
+                <div className="flex my-2 justify-center">
+                    <Button href={`https://indiankanoon.org/?formInput=${msg.Output.FileName}+doctypes:judgments`} as={Link} isExternal color="secondary" variant="ghost" showAnchorIcon> Go to case</Button>
                 </div>
                 <Divider className="my-4" />
             </div>
